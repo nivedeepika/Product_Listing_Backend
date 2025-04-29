2ed18278be3c342738c3cdaabd85d930cfbc841e
nivedeepika <118369201+nivedeepika@users.noreply.github.com>
2025-04-29 11:28:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def signin():
     return jsonify({"message": "Login successful", "name": user["name"]}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
